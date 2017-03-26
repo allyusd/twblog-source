83bcb7318799e87d4adba7640bca8f623848c215
allyusd <allyusd@yahoo.com.tw>
2017-03-26 18:25:23 +0800

    jekyll: update config file

--- _shell/start.sh
+++ _shell/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -e "JEKYLL_ENV=production" --name jekyll --rm -p 4000:4000 -v ~/github/jekyll:/home/ubuntu/jekyll -u $(id -u):$(id -g) allyusd/ubuntu-jekyll bundle exec jekyll serve --host=0.0.0.0 --drafts
+docker run -e "JEKYLL_ENV=production" --name jekyll --rm -p 4000:4000 -v ~/github/jekyll:/home/ubuntu/jekyll -u $(id -u):$(id -g) allyusd/ubuntu-jekyll bundle exec jekyll serve --config _config.yml,_config_dev.yml --host=0.0.0.0 --drafts
