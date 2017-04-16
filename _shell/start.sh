02386f6887c342e8f854a32e5482bed81f76f855
allyusd <allyusd@yahoo.com.tw>
2017-04-16 14:33:27 +0800

    site: set site config for zh-TW

--- _shell/start.sh
+++ _shell/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -e "JEKYLL_ENV=production" --name blog --rm -d -p 4000:4000 -v $(pwd):/home/ubuntu/jekyll -u $(id -u):$(id -g) allyusd/ubuntu-jekyll bundle exec jekyll serve --config _config.yml,_config_dev.yml --host=0.0.0.0 --drafts
+docker run -e "JEKYLL_ENV=production" --name twblog --rm -d -p 4001:4000 -v $(pwd):/home/ubuntu/jekyll -u $(id -u):$(id -g) allyusd/ubuntu-jekyll bundle exec jekyll serve --config _config.yml,_config_dev.yml --host=0.0.0.0 --drafts
